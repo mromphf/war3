ffd4dda0003e588d02e18a6a0ac864979ef383e0
Mark Romphf <markromphf@gmail.com>
2025-03-29 12:29:33 -0600

    Switch to public access for certain quest functions.

--- questing.lua
+++ questing.lua
@@ -17,15 +17,20 @@ do
     ---@type boolean
     local is_orange_unassigned
 
+
+    -- ------------------------------------------
+    -- Scenario-agnostic
+    -- ------------------------------------------
+
     ---@param p player
     ---@return boolean
-    local function IsHumanPlaying(p)
+    function IsHumanPlaying(p)
         return GetPlayerSlotState(p) == PLAYER_SLOT_STATE_PLAYING and
                GetPlayerController(p) == MAP_CONTROL_USER
     end
 
     ---@return force
-    local function AllHumanPlayers()
+    function AllHumanPlayers()
         local f = CreateForce()
 
         for n = 0, MAX_PLAYERS - 1 do
@@ -38,8 +43,17 @@ do
         return f
     end
 
+    ---@param q QuestDefinition
+    ---@param msg string
+    function CompleteQuest(q, msg)
+        if q.data then
+            QuestSetCompleted(q.data, true)
+            QuestMessageBJ(human_players_force, bj_QUESTMESSAGE_COMPLETED, msg)
+        end
+    end
+
     ---@param quest QuestDefinition
-    local function AssignQuest(quest)
+    function AssignQuest(quest)
         if quest.data then
             QuestMessageBJ(human_players_force or AllHumanPlayers(), bj_QUESTMESSAGE_DISCOVERED, quest.message)
             QuestSetDiscovered(quest.data, true)
@@ -48,7 +62,7 @@ do
 
     ---@param definition QuestDefinition
     ---@return quest | nil
-    local function InitQuest(definition)
+    function InitQuest(definition)
         if not definition then
             print("ERR: Tried to init quest with no definition")
             return nil
@@ -82,6 +96,10 @@ do
 
         return lb
     end
+
+    -- ------------------------------------------
+    -- Scenario-specific
+    -- ------------------------------------------
 
     local function AssignRescueQuest()
         if IsQuestDiscovered(quests.rescue.data) then return end
