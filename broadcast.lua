124905e2147fcbc2ae9dd03b847e68a22452f134
Mark Romphf <markromphf@gmail.com>
2025-03-30 07:27:43 -0600

    Introduce Broadcast table to handle quest messaging.

---
+++ broadcast.lua
@@ -0,0 +1,30 @@
+do
+    local Broadcast = {}
+
+    ---@param unitname string
+    function Broadcast.NewUnitAvailable(unitname)
+        QuestMessageBJ(GetPlayersAll(), bj_QUESTMESSAGE_UNITAVAILABLE,
+            "|cff87CEEBNEW UNIT AVAILABLE|r\n" .. unitname)
+    end
+
+    ---@param msg string
+    function Broadcast.Hint(msg)
+        QuestMessageBJ(GetPlayersAll(), bj_QUESTMESSAGE_HINT,
+            "|cff32CD32HINT|r: " .. msg)
+    end
+
+    ---@param q QuestDefinition
+    function Broadcast.QuestDiscovered(q)
+        QuestMessageBJ(GetPlayersAll(), bj_QUESTMESSAGE_DISCOVERED,
+            "|cffffcc00QUEST DICOVERED|r\n" .. q.title)
+    end
+
+
+    ---@param q QuestDefinition
+    function Broadcast.QuestComplete(q)
+        QuestMessageBJ(GetPlayersAll(), bj_QUESTMESSAGE_COMPLETED,
+            "|cffffcc00QUEST COMPLETED|r\n" .. q.title)
+    end
+
+    return Broadcast
+end
