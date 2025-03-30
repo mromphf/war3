b6c19f04557dded07badc0a1a6ca5a07c0145d52
Mark Romphf <markromphf@gmail.com>
2025-03-30 09:31:49 -0600

    Function rename.

--- opponents.lua
+++ opponents.lua
@@ -1,7 +1,7 @@
 do
     ---@param p player
     ---@param state playerstate
-    local function RegisterResourceTrigger(p, state)
+    local function RegisterReplenishTrigger(p, state)
         trig = CreateTrigger()
         TriggerRegisterPlayerStateEvent(trig, p, state, LESS_THAN_OR_EQUAL, 500)
         TriggerAddAction(trig, function()
@@ -10,10 +10,10 @@ do
     end
 
     local function RegisterTriggers()
-        RegisterResourceTrigger(players.green, PLAYER_STATE_RESOURCE_GOLD)
-        RegisterResourceTrigger(players.green, PLAYER_STATE_RESOURCE_LUMBER)
-        RegisterResourceTrigger(players.orange, PLAYER_STATE_RESOURCE_GOLD)
-        RegisterResourceTrigger(players.orange, PLAYER_STATE_RESOURCE_LUMBER)
+        RegisterReplenishTrigger(players.green, PLAYER_STATE_RESOURCE_GOLD)
+        RegisterReplenishTrigger(players.green, PLAYER_STATE_RESOURCE_LUMBER)
+        RegisterReplenishTrigger(players.orange, PLAYER_STATE_RESOURCE_GOLD)
+        RegisterReplenishTrigger(players.orange, PLAYER_STATE_RESOURCE_LUMBER)
     end
 
     local function FireAi()
